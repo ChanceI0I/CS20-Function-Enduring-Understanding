9a71653d40dd3e7cc6b10a25265248ffa1e374f4
Chance <Chengzhi-Yang@outlook.com>
2022-09-27 13:27:35 -0600

    add most of function

---
+++ lib.js
@@ -0,0 +1,104 @@
+function devide() {
+    console.log("------------------------------------------------------")
+}
+
+function greeting(name, timeOfDay) {
+    return `Hello ${name}. Have a great ${timeOfDay}`;
+}
+
+function fahrToCels(fahrTemp) {
+    let celsius = (fahrTemp - 32) * 5/9;
+    return celsius;
+}
+
+function heronsFormula(a, b, c) {
+    let s = (a + b + c)/2;
+    let area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+    return area;
+}
+
+function heightCheck(height) {
+    if (height < 82) {
+        return "Too short for ride"
+    } else {
+        return "All Aboard!"
+    }
+}
+
+function analyzeNumber(number) {
+    if (number > 0) {
+        return "positive"
+    } else if (number < 0) {
+        return "negative"
+    } else if (number == 0) {
+        return "zero"
+    }
+}
+
+function gradeFeedback(grade) {
+    if (grade >= 80) {
+        return "Good job";
+    } else if (grade >= 50) {
+        return "Passable"
+    } else {
+        return "Keep trying"
+    }
+}
+
+function abs(number) {
+    if (number >= 0) {
+        return number
+    } else if (number < 0) {
+        return -number
+    }
+}
+
+function isEven(number) {
+    if (number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isDivisibleBy(number, divisor) {
+    if (number % divisor === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function pointLocation(x, y) {
+    if (x === 0 && y === 0) {
+        return "origin";
+    } else if (x === 0 && y != 0) {
+        return "y-axis";
+    } else if (x != 0 && y === 0) {
+        return "x-axis"
+    } else if ( x > 0 && y > 0) {
+        return "quadrant I"
+    } else if ( x < 0 && y > 0 ) {
+        return "quadrant II"
+    } else if ( x < 0 && y < 0) {
+        return "quadrant III"
+    } else if ( x > 0 && y < 0) {
+        return "quadrant IV"
+    }
+}
+
+function ordinalAdjective(number) {
+    if (number === 1) {
+        return `${number}st`
+    } else if (number === 2) {
+        return `${number}nd`
+    } else if (number === 3) {
+        return `${number}rd`
+    } else {
+        return `${number}th`
+    }
+}
+
+function minPayment(balance) {
+    
+}
---
+++ main.js
@@ -0,0 +1,78 @@
+console.log(greeting("Megan", "Morning"));
+console.log(greeting("Azra", "Afternoon"));
+console.log(greeting("Nathan", "Night"));
+
+devide()
+
+console.log(fahrToCels(32));
+console.log(fahrToCels(68));
+console.log(fahrToCels(0));
+
+devide()
+
+console.log(heronsFormula(8, 9, 10));
+console.log(heronsFormula(3, 4, 5));
+console.log(heronsFormula(10, 10, 10));
+
+devide()
+
+console.log(heightCheck(90));
+console.log(heightCheck(80));
+console.log(heightCheck(82));
+
+devide()
+
+console.log(analyzeNumber(7));
+console.log(analyzeNumber(-15));
+console.log(analyzeNumber(0));
+
+devide()
+
+console.log(gradeFeedback(80));
+console.log(gradeFeedback(50));
+console.log(gradeFeedback(35));
+
+devide()
+
+console.log(abs(7));
+console.log(abs(-15));
+console.log(abs(0));
+console.log(abs(-7));
+
+devide()
+
+console.log(isEven(10));
+console.log(isEven(9));
+console.log(isEven(0));
+console.log(isEven(-7));
+
+devide()
+
+console.log(isDivisibleBy(10,5));
+console.log(isDivisibleBy(10,6));
+console.log(isDivisibleBy(21,-7));
+
+devide()
+
+console.log(pointLocation(0, 0));
+console.log(pointLocation(5, 0));
+console.log(pointLocation(0, -3));
+console.log(pointLocation(2, 4));
+console.log(pointLocation(-5, 7));
+console.log(pointLocation(-1, -8));
+console.log(pointLocation(7, -6));
+
+devide()
+
+console.log(ordinalAdjective(1));
+console.log(ordinalAdjective(2));
+console.log(ordinalAdjective(3));
+console.log(ordinalAdjective(4));
+console.log(ordinalAdjective(5));
+console.log(ordinalAdjective(6));
+console.log(ordinalAdjective(7));
+console.log(ordinalAdjective(8));
+console.log(ordinalAdjective(9));
+
+devide()
+
